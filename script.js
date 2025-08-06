27def2ed4e0d8969f20a3cbc602fde4348299490
Marshall0x <140571045+HassanJr69@users.noreply.github.com>
2025-08-06 02:14:40 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const container = document.getElementById("graph-container");
+  container.innerHTML = "✨ Graph will be rendered here soon...";
+});
